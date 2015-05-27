4935794436d8fbce14790ee07d43ecc3e97bfae9
frombegin <frombegin@gmail.com>
2015-05-27 16:28:50 +0800

    add client files.

---
+++ cc/client/session.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Session(object):
+    def __init__(self):
+        super(Session, self).__init__()
+        self.__id = None
+
+    def is_anoymous(self):
+        return self.__id is None
+
+    def login(self, identify, password):
+        pass
+
+    def logout(self):
+        pass
+
+    def register(self, identify, password):
+        pass
+
+    def get_book_list(self, key, data):
+        # key: top, tag, user
+        # data: (popular, newest), tag, user_id
+        pass
+
+    def get_tag_cloud(self):
+        pass
+
+    def search(self, query):
+        pass
+
