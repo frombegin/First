acf5a0f79d30bad8cc850a64c62807c899090212
frombegin <frombegin@gmail.com>
2015-05-27 20:02:08 +0800

    more scaffold

--- cc/core/block.py
+++ cc/core/block.py
@@ -16,17 +16,42 @@ class Block(object):
             parent.add_child(self)
         self.__parent = parent
 
-    def _accept_child(self, block):
+    def _accept(self, block):
         assert isinstance(block, Block)
         return False
 
+    def _changed(self):
+        pass
+
     def add_child(self, block):
-        if self._accept_child(block):
-            pass
+        if self._accept(block):
+            self.__children.append(block)
+            self._changed()
 
     def remove_child(self, block):
         assert isinstance(block, Block)
-        pass
+        self.__children.remove(block)
+
+    @property
+    def parent(self):
+        return self.__parent
+
+    @property
+    def level(self):
+        result = 0
+        p = self.__parent
+        while p:
+            p = p.__parent
+            result += 1
+        return result
+
+    @property
+    def bounds(self):
+        return self.__bounds
+
+    @property
+    def margin(self):
+        return self.__margin
 
     def __str__(self):
         pass
